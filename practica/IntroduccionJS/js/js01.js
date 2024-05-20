286681cab53848b90b5371dd221af1c401bd2222
alejandraob <alejandraboisier@hotmail.com>
2024-05-20 16:07:21 -0300

    aprendiend js 1

---
+++ practica/IntroduccionJS/js/js01.js
@@ -0,0 +1,9 @@
+///VARIABLES
+//se inicia variable y se asigna valor;
+var producto='audifonos gamer';
+
+var disponible; //iniciada la vaiable pero sin valor asignado
+
+producto=true; //reasignando el valor de la variable
+
+
