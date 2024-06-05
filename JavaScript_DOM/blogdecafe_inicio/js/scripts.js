4ba1bf35556d57acbea645521099ecea045e0860
alejandraob <alejandraboisier@hotmail.com>
2024-06-05 08:09:12 -0300

    js

---
+++ JavaScript_DOM/blogdecafe_inicio/js/scripts.js
@@ -0,0 +1,13 @@
+//querySelector
+
+const heading=document.querySelector('h2'); //selecciona el primer h2
+//console.log(heading);
+heading.textContent='Nuevo Heading'; //cambia el texto del h2
+
+
+//querySelectorAll
+
+
+
+
+//queryEmelentById
